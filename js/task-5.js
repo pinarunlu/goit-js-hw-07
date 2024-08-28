1b572ca184664ebfcaea2f726946a5cc7b6f7cad
pınar <pinar.dogans96@gmail.com>
2024-08-29 00:01:55 +0300

    task 5 oluşturuldu

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const changeColorButton = document.querySelector('.change-color');
+const colorDisplay = document.querySelector('.color');
+
+
+changeColorButton.addEventListener('click', () => {
+  const randomColor = getRandomHexColor(); 
+  document.body.style.backgroundColor = randomColor; 
+  colorDisplay.textContent = randomColor; 
+});
