d33c0ee38c1e756818efa98755671275c91387aa
pınar <pinar.dogans96@gmail.com>
2024-08-29 01:31:16 +0300

    task 6 oluşturuldu

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,52 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+const controls = document.getElementById('controls');
+const input = controls.querySelector('input');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+
+createButton.addEventListener('click', () => {
+  const amount = parseInt(input.value, 10);
+
+ 
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = ''; 
+  } else {
+    alert('Please enter a number between 1 and 100.');
+  }
+});
+
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+
+function createBoxes(amount) {
+  destroyBoxes(); 
+
+  const boxes = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = '5px';
+    boxes.push(box);
+    size += 10; 
+  }
+
+  boxesContainer.append(...boxes); 
+}
+
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = ''; 
+}
